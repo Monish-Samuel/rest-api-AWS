22789869b49a0625caf3d7d2599ca7f7d0eb478f
Monish-Samuel <samuelmonish1@gmail.com>
2021-03-16 12:02:33 +0530

    new files added

--- issues_api.py
+++ issues_api.py
@@ -1,28 +1,71 @@
 from flask import Flask, render_template, request
-import requests, json
+import requests
 
-app = Flask(__name__, template_folder='template')
+app = Flask(__name__, template_folder='templates')
 
 
 @app.route('/')
-def hello_world():
+def home_page():
     return render_template('home.html')
 
 
-@app.route('/', methods=['POST'])
-def matching_char():
-    numb = request.form['number']
-    url = "https://api.github.com/repos/MOnish-Samuel/rest-api/issues/"+numb
+@app.route('/home', methods=['POST'])
+def home_redirect_view():
+    if request.form.get("view.html"):
+        return render_template('view.html')
+    elif request.form.get("update.html"):
+        return render_template('update.html')
+    else:
+        return render_template('create.html')
 
+
+@app.route('/view', methods=['POST'])
+def issue_viewer():
+    numb1 = request.form['number']
+    url = "https://api.github.com/repos/Monish-Samuel/rest-api/issues/"+numb1
     payload = {}
     headers = {
-        'Authorization': 'Bearer 412102ffa2f1dd81b165427dfb7ce29b3592ea7b',
+        'Authorization': 'Bearer 3b2dae32edb4f80ac73f2fe5e624123a1b53e6dd',
         'Cookie': '_octo=GH1.1.906356758.1615379192; logged_in=no'
     }
 
-    response = requests.request("GET", url, headers=headers, data=payload)
-    order = response.json()
-    return render_template('pass1.html', r=order)
+    response = requests.request("GET", url, headers=headers, data=payload).json()
+    return render_template('pass1.html', r=response)
+
+
+@app.route('/create', methods=['POST'])
+def issue_creator():
+    title = request.form['title']
+    body = request.form['issue']
+    url = "https://api.github.com/repos/Monish-Samuel/rest-api/issues"
+
+    payload = "{\r\n    \"title\": \""+title+"\",\r\n    \"body\": \""+body+"\"\r\n}"
+    headers = {
+        'Authorization': 'Bearer 3b2dae32edb4f80ac73f2fe5e624123a1b53e6dd',
+        'Content-Type': 'application/json',
+        'Cookie': '_octo=GH1.1.906356758.1615379192; logged_in=no'
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload).json()
+    return render_template('pass1.html', r=response)
+
+
+@app.route('/update', methods=['POST'])
+def issue_updater():
+    numb = request.form['number']
+    title = request.form['title']
+    body = request.form['issue']
+    url = "https://api.github.com/repos/Monish-Samuel/rest-api/issues/"+numb
+
+    payload = "{\r\n    \"title\": \""+title+"\",\r\n    \"body\": \""+body+"\"\r\n}"
+    headers = {
+        'Authorization': 'Bearer 3b2dae32edb4f80ac73f2fe5e624123a1b53e6dd',
+        'Content-Type': 'application/json',
+        'Cookie': '_octo=GH1.1.906356758.1615379192; logged_in=no'
+    }
+
+    response = requests.request("PATCH", url, headers=headers, data=payload).json()
+    return render_template('pass1.html', r=response)
 
 
 if __name__ == '__main__':
